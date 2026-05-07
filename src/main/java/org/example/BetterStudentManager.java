a66171e73770fadfba4e383fff293ad15d3b7c38
Navya-git-account <bommishettinavya@gmail.com>
2026-05-06 20:22:39 -0500

    added code to the studentManger

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -7,15 +7,32 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class BetterStudentManager {
     public List<Student> students = new ArrayList<>();
 
     public void addStudent(String name, double grade) {
         // This method should add a new student to the list of students
+        Student newStudent = new Student();
+        newStudent.name = name;
+        newStudent.grade = grade;
+        students.add(newStudent);
     }
 
     public void removeStudent(String name) {
         // This method should remove the student with the given name from the list of students
+
+        int indexToRemove = -1;
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).name.equals(name)) {
+                indexToRemove = i;
+                break;
+            }
+        }
+        if (indexToRemove != -1) {
+            students.remove(indexToRemove);
+        }
     }
 
     public String getStudentList() {
@@ -29,6 +46,13 @@ public class BetterStudentManager {
         // looping through the list of students
         //
         // Replace the following line with your implementation
-        return null;
+        StringBuilder studentData = new StringBuilder();
+        for (Student student: students){
+            studentData.append(student.name + " " + student.grade + "\n");
+
+        }
+        return studentData.toString();
+
+
     }
 }
